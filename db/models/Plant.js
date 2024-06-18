ab6cce1e7d287f55e7872bf4252345cf6a94f36e
Sabine Pfanner <pfanner.sabine@gmail.com>
2024-06-18 12:06:19 +0200

    adapt MongoDB schema and add timestamps

--- db/models/Plant.js
+++ db/models/Plant.js
@@ -4,43 +4,51 @@ const { Schema } = mongoose;
 
 // Define the nested schema for 'seed'
 // Courtesy of chatGPT
-const seedSchema = new Schema({
-  start: {
-    type: Schema.Types.Mixed,
-    default: null
+const seedSchema = new Schema(
+  {
+    start: {
+      type: Schema.Types.Mixed,
+      default: null,
+    },
+    end: {
+      type: Schema.Types.Mixed,
+      default: null,
+    },
   },
-  end: {
-    type: Schema.Types.Mixed,
-    default: null
-  }
-}, { _id: false });
+  { _id: false }
+);
 
 // Define the schema for 'tasks'
-const tasksSchema = new Schema({
-  seed: {
-    type: seedSchema,
-    default: { start: null, end: null } 
-  }
-}, { _id: false });
-
-const plantSchema = new Schema({
-  name: { type: String, required: true },
-  botanicalName: { type: String, required: true },
-  waterDemand: { type: String, required: true },
-  nutrientDemand: { type: String, required: true },
-  growingConditions: { type: String, required: true },
-  placement: { type: String, required: true },
-  perennial: { type: Boolean, required: true },
-  frostSensitive: { type: Boolean, required: true },
-  cropType: { type: String, required: true },
-  tasks: {
-    type: tasksSchema,
-    default: { seed: { start: null, end: null }}
+const tasksSchema = new Schema(
+  {
+    seed: {
+      type: seedSchema,
+      default: { start: null, end: null },
+    },
   },
-  // tasks: { type: String },
-  image: { type: String },
-  owner: { type: String, required: true },
-});
+  { _id: false }
+);
+
+const plantSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    botanicalName: { type: String, required: true },
+    waterDemand: { type: String, required: true },
+    nutrientDemand: { type: String, required: true },
+    growingConditions: { type: String, required: true },
+    placement: { type: String, required: true },
+    perennial: { type: Boolean, required: true },
+    frostSensitive: { type: Boolean, required: true },
+    cropType: { type: String, required: true },
+    tasks: {
+      type: tasksSchema,
+      default: { seed: { start: null, end: null } },
+    },
+    image: { type: String },
+    owner: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const Plant = mongoose.models.Plant || mongoose.model("Plant", plantSchema);
 
